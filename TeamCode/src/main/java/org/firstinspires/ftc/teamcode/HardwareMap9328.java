5e5be67cdce6024431a3692bc57619a5747afade
Josef Baltz <josef@baltz.net>
2017-12-12 15:31:40 -0600

    HardwareMap Setup
    All Motors are now configured

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/HardwareMap9328.java
@@ -0,0 +1,39 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+import com.qualcomm.robotcore.util.ElapsedTime;
+
+public class HardwareMap9328 {
+    public DcMotor leftTread;
+    public DcMotor rightTread;
+    public DcMotor clawWinch;
+    public DcMotor claw;
+
+    HardwareMap hwMapv2;
+    private ElapsedTime period = new ElapsedTime();
+
+    public HardwareMap9328() {
+    }
+
+    public void init(HardwareMap hwMap){
+        hwMapv2 = hwMap;
+
+        leftTread   = hwMapv2.get(DcMotor.class, "LeftTread");
+        rightTread  = hwMapv2.get(DcMotor.class, "RightTread");
+        leftTread.setDirection(DcMotor.Direction.REVERSE);
+        clawWinch   = hwMapv2.get(DcMotor.class, "Winch");
+        claw        = hwMapv2.get(DcMotor.class, "Claw");
+
+        leftTread.setPower(0);
+        rightTread.setPower(0);
+        claw.setPower(0);
+        clawWinch.setPower(0);
+
+        leftTread.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        rightTread.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        claw.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        clawWinch.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+    }
+}

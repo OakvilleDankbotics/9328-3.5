62e22aea4fb16a0845308dd4ed09f671115659db
Josef Baltz <josef@baltz.net>
2017-12-19 09:31:38 -0600

    Code Cleanup
    Whitespace cleanup
    Variable Changes (Public to Private Stuff)
    Inlineing Variables (Don't know exactly where one variable went)

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/HardwareMap9328.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/HardwareMap9328.java
@@ -1,37 +1,24 @@
 package org.firstinspires.ftc.teamcode;
-
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.HardwareMap;
-import com.qualcomm.robotcore.util.ElapsedTime;
-
-public class HardwareMap9328 {
-    public DcMotor leftTread;
-    public DcMotor rightTread;
-    public DcMotor clawWinch;
-    public DcMotor claw;
-    public int clawOpenPosition = 0;
-    public int clawClosedPosition = 45;
-
-    HardwareMap hwMapv2;
-    private ElapsedTime period = new ElapsedTime();
-
-    public HardwareMap9328() {
-    }
-
-    public void init(HardwareMap hwMap){
-        hwMapv2 = hwMap;
-
-        leftTread   = hwMapv2.get(DcMotor.class, "LeftTread");
-        rightTread  = hwMapv2.get(DcMotor.class, "RightTread");
+class HardwareMap9328 {
+    DcMotor leftTread;
+    DcMotor rightTread;
+    DcMotor clawWinch;
+    DcMotor claw;
+    int clawOpenPosition = 0;
+    int clawClosedPosition = 45;
+    HardwareMap9328() {}
+    void init(HardwareMap hwMap){
+        leftTread   = hwMap.get(DcMotor.class, "LeftTread");
+        rightTread  = hwMap.get(DcMotor.class, "RightTread");
         rightTread.setDirection(DcMotor.Direction.REVERSE);
-        clawWinch   = hwMapv2.get(DcMotor.class, "Winch");
-        claw        = hwMapv2.get(DcMotor.class, "Claw");
-
+        clawWinch   = hwMap.get(DcMotor.class, "Winch");
+        claw        = hwMap.get(DcMotor.class, "Claw");
         leftTread.setPower(0);
         rightTread.setPower(0);
         claw.setPower(0);
         clawWinch.setPower(0);
-
         leftTread.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
         rightTread.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
         claw.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/SingleDriverLinear.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/SingleDriverLinear.java
@@ -1,8 +1,6 @@
 package org.firstinspires.ftc.teamcode;
-
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
-
 @TeleOp(name="9328: Linear Single Driver", group="9328")
 public class SingleDriverLinear extends LinearOpMode {
     private HardwareMap9328 hwMap = new HardwareMap9328();
@@ -10,17 +8,14 @@ public class SingleDriverLinear extends LinearOpMode {
     private int clawClosedPosition = new HardwareMap9328().clawClosedPosition;
     private double leftStick = gamepad1.left_stick_y, rightStick = gamepad1.right_stick_y;
     private boolean clawState = false;
-
     @Override
     public void runOpMode() {
         hwMap.init(hardwareMap);
         waitForStart();
-
         hwMap.claw.setTargetPosition(45);
         while (opModeIsActive()) {
             hwMap.leftTread.setPower(leftStick);
             hwMap.rightTread.setPower(rightStick);
-
             if (gamepad1.dpad_up) {
                 hwMap.clawWinch.setPower(0.75);
             } else if (gamepad1.dpad_down) {

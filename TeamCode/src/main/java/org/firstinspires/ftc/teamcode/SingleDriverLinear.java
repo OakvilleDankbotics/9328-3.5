90392b1782fc4eb50976ca102bdc4bee10e1ed63
Josef Baltz <josef@baltz.net>
2018-01-12 10:04:24 -0600

    Code Cleaned

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/SingleDriverLinear.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/SingleDriverLinear.java
@@ -4,8 +4,6 @@ import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 @TeleOp(name="9328: Linear Single Driver", group="9328")
 public class SingleDriverLinear extends LinearOpMode {
     private HardwareMap9328 hwMap = new HardwareMap9328();
-    private int clawOpenPosition = new HardwareMap9328().clawOpenPosition;
-    private int clawClosedPosition = new HardwareMap9328().clawClosedPosition;
     private double leftStick = gamepad1.left_stick_y, rightStick = gamepad1.right_stick_y;
     private boolean clawState = false;
     @Override
@@ -21,10 +19,10 @@ public class SingleDriverLinear extends LinearOpMode {
             } else if (gamepad1.dpad_down) {
                 hwMap.clawWinch.setPower(-0.75);
             } else if (gamepad1.a & !clawState) {
-                hwMap.claw.setTargetPosition(clawOpenPosition);
+                hwMap.claw.setTargetPosition(hwMap.clawOpenPosition);
                 clawState = true;
             } else if (gamepad1.a & clawState) {
-                hwMap.claw.setTargetPosition(clawClosedPosition);
+                hwMap.claw.setTargetPosition(hwMap.clawClosedPosition);
                 clawState = false;
             } else {
                 hwMap.clawWinch.setPower(0);
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOpLinear.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOpLinear.java
@@ -4,8 +4,6 @@ import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 @TeleOp(name="9328: TeleOp Linear", group="9328")
 public class TeleOpLinear extends LinearOpMode {
     private HardwareMap9328 hwMap = new HardwareMap9328();
-    private int clawOpenPosition = new HardwareMap9328().clawOpenPosition;
-    private int clawClosedPosition = new HardwareMap9328().clawClosedPosition;
     private double leftStick1 = gamepad1.left_stick_y, rightStick1 = gamepad1.right_stick_y, rightStick2 = gamepad2.right_stick_y;
     private boolean clawState = false;
     @Override
@@ -19,10 +17,10 @@ public class TeleOpLinear extends LinearOpMode {
             if (rightStick2 > 0.2 || rightStick2 < -0.2) {
                 hwMap.clawWinch.setPower(rightStick2);
             } else if (gamepad2.right_stick_button & !clawState) {
-                hwMap.claw.setTargetPosition(clawOpenPosition);
+                hwMap.claw.setTargetPosition(hwMap.clawOpenPosition);
                 clawState = true;
             } else if (gamepad2.right_stick_button & clawState) {
-                hwMap.claw.setTargetPosition(clawClosedPosition);
+                hwMap.claw.setTargetPosition(hwMap.clawClosedPosition);
                 clawState = false;
             } else {
                 hwMap.clawWinch.setPower(0);
